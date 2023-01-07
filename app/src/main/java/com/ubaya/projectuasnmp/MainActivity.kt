412a8a18fa5d0e3914afea2d0c3f3a36ac88b7d1
ToastedBaguette <ronyhartonoirawan@gmail.com>
2023-01-08 01:35:13 +0700

    Update Navigation Drawer

--- app/src/main/java/com/ubaya/projectuasnmp/MainActivity.kt
+++ app/src/main/java/com/ubaya/projectuasnmp/MainActivity.kt
@@ -1,10 +1,18 @@
 package com.ubaya.projectuasnmp
 
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.widget.ViewPager2
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.card_meme.view.*
+import kotlinx.android.synthetic.main.drawer_header.view.*
+import kotlinx.android.synthetic.main.drawer_layout.*
+import kotlinx.android.synthetic.main.drawer_layout.view.*
 
 class MainActivity : AppCompatActivity() {
     val fragments:ArrayList<Fragment> = ArrayList()
@@ -12,10 +20,48 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //set up drawer layout
+        setContentView(R.layout.drawer_layout)
+        supportActionBar?.setDisplayHomeAsUpEnabled(false)
+        var drawerToggle =
+            ActionBarDrawerToggle(this, drawerLayout, toolbar,R.string.app_name,
+                R.string.app_name)
+        drawerToggle.isDrawerIndicatorEnabled = true
+        drawerToggle.syncState()
+
+        var sharedFile = "com.ubaya.projectuasnmp"
+        var shared: SharedPreferences = this.getSharedPreferences(sharedFile,
+            Context.MODE_PRIVATE )
+
+        val headView = navView.getHeaderView(0)
+        headView.txtFullName.text = shared.getString("firstName", "") + " " + shared.getString("lastName", "")
+        headView.txtUsername.text = shared.getString("username", "")
+
+        val urlProf = "https://images.unsplash.com/photo-1529665253569-6d01c0eaf7b6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1085&q=80"
+        Picasso.get().load(urlProf).into(headView.imgProfile)
+
+        val urlBack = "https://www.tutorialspoint.com/opencv/images/gaussian_blur.jpg"
+        Picasso.get().load(urlBack).into(headView.imgBackground)
+
+        navView.setNavigationItemSelectedListener {
+            viewpager.currentItem  = when(it.itemId) {
+                R.id.itemHome -> 0
+                R.id.itemMyCreation -> 1
+                R.id.itemLeaderboard -> 2
+                R.id.itemSettings -> 3
+                else -> 0
+            }
+            true
+        }
+
+
+
         fragments.add(HomeFragment())
         fragments.add(MyCreationFragment())
         fragments.add(LeaderboardFragment())
         fragments.add(SettingsFragment())
+
 
         val adapter = MenuAdapter(this, fragments)
         viewpager.adapter = adapter
--- app/src/main/java/com/ubaya/projectuasnmp/SettingsFragment.kt
+++ app/src/main/java/com/ubaya/projectuasnmp/SettingsFragment.kt
@@ -28,14 +28,7 @@ import kotlinx.android.synthetic.main.fragment_settings.*
 import org.json.JSONObject
 import java.util.*
 
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
 class SettingsFragment : Fragment() {
-    private var param1: String? = null
-    private var param2: String? = null
-    private val REQUEST_CODE_GALLERY = 1
-    private val REQUEST_CODE_CAMERA = 2
     var userId:Int = 0
     var userName = ""
     var firstName = ""
@@ -82,13 +75,6 @@ class SettingsFragment : Fragment() {
     override fun onAttach(context: Context) {
         super.onAttach(context)
         refreshSharedPreferences(context)
-    }
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
@@ -179,17 +165,5 @@ class SettingsFragment : Fragment() {
             activity?.finish()
         }
         return v
-    }
-
-
-    companion object {
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SettingsFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
     }
 }
