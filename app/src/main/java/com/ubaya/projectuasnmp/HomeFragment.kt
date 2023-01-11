cdab06e15b2e68c29441e72f4c3e149684672347
Victor Manuel Soesilo <79180895+victorman01@users.noreply.github.com>
2023-01-11 22:32:23 +0700

    sorting done
    fab paling kiri ->kanan
    1. clear sort
    2. like sort
    3. comment sort

--- app/src/main/java/com/ubaya/projectuasnmp/HomeFragment.kt
+++ app/src/main/java/com/ubaya/projectuasnmp/HomeFragment.kt
@@ -9,9 +9,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.android.volley.Request
+import com.android.volley.Response
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.google.android.material.floatingactionbutton.FloatingActionButton
@@ -26,8 +28,7 @@ class HomeFragment : Fragment() {
     override fun onAttach(context: Context) {
         super.onAttach(context)
         var sharedFile = "com.ubaya.projectuasnmp"
-        var shared: SharedPreferences = context.getSharedPreferences(sharedFile,
-            Context.MODE_PRIVATE )
+        var shared: SharedPreferences = context.getSharedPreferences(sharedFile, Context.MODE_PRIVATE )
 
         userId = shared.getInt("userId",0)
     }
@@ -81,10 +82,127 @@ class HomeFragment : Fragment() {
     ): View? {
         var view = inflater.inflate(R.layout.fragment_home, container, false)
         var fab : FloatingActionButton? = view.findViewById(R.id.fab)
+        var fabComment : FloatingActionButton? = view.findViewById(R.id.fabSortComment)
+        var fabLike :FloatingActionButton? = view.findViewById(R.id.fabSortLike)
+        var fabClear :FloatingActionButton? = view.findViewById(R.id.fabSortClear)
+
+        fabComment?.setOnClickListener(){
+            val q = Volley.newRequestQueue(activity)
+            val url = "https://ubaya.fun/native/160420041/get_memes.php"
+            var stringRequest = object:StringRequest(
+                Request.Method.POST, url,
+                Response.Listener<String> {
+                    Log.d("apiresult", it)
+                    val obj = JSONObject(it)
+                    if(obj.getString("result")=="OK"){
+                        val data = obj.getJSONArray("data")
+                        memes.clear()
+                        for(i in 0 until data.length()) {
+                            val memeObj = data.getJSONObject(i)
+                            val meme = Meme(
+                                memeObj.getInt("idmemes"),
+                                memeObj.getString("image_url"),
+                                memeObj.getString("top_text"),
+                                memeObj.getString("bottom_text"),
+                                memeObj.getInt("num_likes"),
+                                memeObj.getInt("users_id"),
+                                memeObj.getInt("total_comments")
+                            )
+                            memes.add(meme)
+                        }
+                        updateList()
+                    }
+                },
+                Response.ErrorListener {
+                    Log.e("apiresult", it.message.toString())
+                })
+            {
+                override fun getParams() = hashMapOf(
+                    "sort" to "comment",
+                )
+            }
+            q.add(stringRequest)
+        }
+        fabLike?.setOnClickListener(){
+            val q = Volley.newRequestQueue(activity)
+            val url = "https://ubaya.fun/native/160420041/get_memes.php"
+            var stringRequest = object:StringRequest(
+                Request.Method.POST, url,
+                Response.Listener<String> {
+                    Log.d("apiresult", it)
+                    val obj = JSONObject(it)
+                    if(obj.getString("result")=="OK"){
+                        val data = obj.getJSONArray("data")
+                        memes.clear()
+                        for(i in 0 until data.length()) {
+                            val memeObj = data.getJSONObject(i)
+                            val meme = Meme(
+                                memeObj.getInt("idmemes"),
+                                memeObj.getString("image_url"),
+                                memeObj.getString("top_text"),
+                                memeObj.getString("bottom_text"),
+                                memeObj.getInt("num_likes"),
+                                memeObj.getInt("users_id"),
+                                memeObj.getInt("total_comments")
+                            )
+                            memes.add(meme)
+                        }
+                        updateList()
+                    }
+                },
+                Response.ErrorListener {
+                    Log.e("apiresult", it.message.toString())
+                })
+            {
+                override fun getParams() = hashMapOf(
+                    "sort" to "like",
+                )
+            }
+            q.add(stringRequest)
+        }
+        fabClear?.setOnClickListener(){
+            val q = Volley.newRequestQueue(activity)
+            val url = "https://ubaya.fun/native/160420041/get_memes.php"
+            var stringRequest = object:StringRequest(
+                Request.Method.POST, url,
+                Response.Listener<String> {
+                    Log.d("apiresult", it)
+                    val obj = JSONObject(it)
+                    if(obj.getString("result")=="OK"){
+                        val data = obj.getJSONArray("data")
+                        memes.clear()
+                        for(i in 0 until data.length()) {
+                            val memeObj = data.getJSONObject(i)
+                            val meme = Meme(
+                                memeObj.getInt("idmemes"),
+                                memeObj.getString("image_url"),
+                                memeObj.getString("top_text"),
+                                memeObj.getString("bottom_text"),
+                                memeObj.getInt("num_likes"),
+                                memeObj.getInt("users_id"),
+                                memeObj.getInt("total_comments")
+                            )
+                            memes.add(meme)
+                        }
+                        updateList()
+                    }
+                },
+                Response.ErrorListener {
+                    Log.e("apiresult", it.message.toString())
+                })
+            {
+                override fun getParams() = hashMapOf(
+                    "sort" to "clear",
+                )
+            }
+            q.add(stringRequest)
+        }
+
         fab?.setOnClickListener() {
             val intent = Intent(activity, AddMemeActivity::class.java)
             activity?.startActivity(intent)
         }
+
         return view
     }
 
